0d3034e8a10c023bdba531b1b348ac19a1a04199
Chin-Hao Ou <chin-hao.ou@n4group.de>
2017-05-22 13:13:31 +0200

    changes for chapter 9 angular 2 guide

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,12 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroListComponent } from './hero-list.component';
 
-/*import { Logger } from './logger.service';
-import { HeroService } from './hero.service';*/
+import { Logger } from './logger.service';
+/*import { HeroService } from './hero.service';*/
 
 @NgModule({
   imports: [ BrowserModule, FormsModule, HttpModule ],
-  //providers: [ /*BackendService,*/ HeroService, Logger ],
+  providers: [ /*BackendService, HeroService*/, Logger ],
   declarations: [ AppComponent, HeroesComponent, HeroListComponent ],
   bootstrap: [ AppComponent ]
 })
--- src/app/logger.service.ts
+++ src/app/logger.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
 
 @Injectable()
-export class Logger {
+/*export class Logger {
   log(msg: any) { console.log(msg); }
   error(msg: any) { console.error(msg); }
   warn(msg: any) { console.warn(msg); }
+}*/
+
+export class Logger {
+  logs: string[] = [];
+
+  log(message: string) {
+    this.logs.push(message);
+    console.log(message);
+  }
 }
